b3f08334efafc13dd9f8da68301dbc30a5993b8d
caoliangsong <404508644@qq.com>
2017-05-19 14:17:00 +0800

    remove

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const config = require('./task.config.js')
 const exec = require('child_process').exec
 
+let Store = {
+    getAll() {
+        return config
+    },
+    getItem(index) {
+        let len = config.length
+        index = index > len ? 　len : index
+        index = index < 0 ? 0 : index
+        return config[index]
+    },
+    setItem() {},
+    removeItem() {},
+    clear() {}
+}
 config.forEach((item, index) => {
     console.log(`Starting ${item.appName}...`)
     setTimeout(() => {
